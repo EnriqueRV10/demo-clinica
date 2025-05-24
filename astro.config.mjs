e9f605d2ea75242401998947311f525a6011601b
EnriqueRV <sergioenri2001@gmail.com>
2025-05-24 10:00:51 -0600

    fix: normalize import statements and add site target configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+
+const siteTarget = process.env.SITE || "dental"; // valor por defecto
 
 // https://astro.build/config
 export default defineConfig({
+  outDir: `dist/${siteTarget}`,
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
